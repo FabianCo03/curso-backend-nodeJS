ed2ed24293a7cfe25026c863f555d39c3966b0a1
Fabián Orjuela <fabianorjuela@Fabians-MacBook-Pro.local>
2023-04-01 21:30:35 -0500

    feat: 🎸 Clase 03: instalacion express y primer servidor HTTP

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("my-lab");
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send(
+    `Te encuentras en el servidor de Fabián. El número de puerto es ${port}`
+  );
+});
+
+app.listen(port, () => {
+  console.log(`Mi puerto ${port}`);
+});
