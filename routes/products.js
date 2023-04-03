79dd4307e8fb99d7f9e014d5d4e0ac903074dfc2
Fabián Orjuela <fabianorjuela@Fabians-MacBook-Pro.local>
2023-04-03 13:37:32 -0500

    feat: 🎸 Clase 15: Crear, editar y eliminar

--- routes/products.js
+++ routes/products.js
@@ -1,20 +1,10 @@
 const express = require('express');
-const { faker } = require('@faker-js/faker');
+const ProductsService = require('../services/productService');
 const router = express.Router();
+const service = new ProductsService();
 
 router.get('/', (req, res) => {
-  const products = [];
-  const { size } = req.query;
-  //Si no le asigno un tamaño, me van a aparecer por defecto 5 productos. Pero si en la URL pongo ?size=6 me va a mostrar 6 productos
-  const limit = size || 5;
-
-  for (let index = 0; index < limit; index++) {
-    products.push({
-      name: faker.commerce.productName(),
-      price: parseInt(faker.commerce.price(), 10),
-      image: faker.image.imageUrl(),
-    });
-  }
+  const products = service.find();
   res.json(products);
 });
 
@@ -25,25 +15,14 @@ router.get('/filter', (req, res) => {
 
 router.get('/:id', (req, res) => {
   const { id } = req.params;
-  if (id === '999') {
-    res.status(404).json({
-      message: 'Not found',
-    });
-  } else {
-    res.status(200).json({
-      id,
-      name: 'producto 2',
-      price: 2300,
-    });
-  }
+  const product = service.findOnlyOne(id);
+  res.json(product);
 });
 
 router.post('/', (req, res) => {
   const body = req.body;
-  res.status(201).json({
-    message: 'created',
-    data: body,
-  });
+  const newProduct = service.create(body);
+  res.status(201).json(newProduct);
 });
 
 router.put('/', (req, res) => {
@@ -59,19 +38,14 @@ router.put('/', (req, res) => {
 router.patch('/:id', (req, res) => {
   const { id } = req.params;
   const body = req.body;
-  res.json({
-    message: 'update Patch',
-    data: body,
-    id,
-  });
+  const product = service.update(id, body);
+  res.json(product);
 });
 
 router.delete('/:id', (req, res) => {
   const { id } = req.params;
-  res.json({
-    message: 'delete',
-    id,
-  });
+  const rta = service.delete(id);
+  res.json(rta);
 });
 
 module.exports = router;
