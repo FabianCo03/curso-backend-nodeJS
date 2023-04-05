3d893355bbecc1de408cac9314f10726ea9251f4
Fabián Orjuela <fabianorjuela@Fabians-MacBook-Pro.local>
2023-04-05 18:53:46 -0500

    feat: 🎸 Clase 16: Async await y captura de errores (trycatch)

--- routes/products.js
+++ routes/products.js
@@ -3,8 +3,8 @@ const ProductsService = require('../services/productService');
 const router = express.Router();
 const service = new ProductsService();
 
-router.get('/', (req, res) => {
-  const products = service.find();
+router.get('/', async (req, res) => {
+  const products = await service.find();
   res.json(products);
 });
 
@@ -13,38 +13,44 @@ router.get('/filter', (req, res) => {
   res.send('yo soy un filter');
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   const { id } = req.params;
-  const product = service.findOnlyOne(id);
+  const product = await service.findOnlyOne(id);
   res.json(product);
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   const body = req.body;
-  const newProduct = service.create(body);
+  const newProduct = await service.create(body);
   res.status(201).json(newProduct);
 });
 
-router.put('/', (req, res) => {
-  const { id } = req.params;
-  const body = req.body;
-  res.json({
-    message: 'update',
-    data: body,
-    id,
-  });
+// router.put('/', (req, res) => {
+//   const { id } = req.params;
+//   const body = req.body;
+//   res.json({
+//     message: 'update',
+//     data: body,
+//     id,
+//   });
+// });
+
+router.patch('/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const body = req.body;
+    const product = await service.update(id, body);
+    res.json(product);
+  } catch (error) {
+    res.status(404).json({
+      message: `${error} PATCH`,
+    });
+  }
 });
 
-router.patch('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   const { id } = req.params;
-  const body = req.body;
-  const product = service.update(id, body);
-  res.json(product);
-});
-
-router.delete('/:id', (req, res) => {
-  const { id } = req.params;
-  const rta = service.delete(id);
+  const rta = await service.delete(id);
   res.json(rta);
 });
 
