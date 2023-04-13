a51a03f74e270f3be9f649f4e8ce4d6ae74ccdb4
Fabián Orjuela <fabianorjuela@Fabians-MacBook-Pro.local>
2023-04-12 22:12:36 -0500

    feat: 🎸 Clase 21: Probando endpoints

--- middlewares/validator.handler.js
+++ middlewares/validator.handler.js
@@ -4,14 +4,8 @@ const boom = require('@hapi/boom');
 function validatorHandler(schema, property) {
   //closures
   return (req, res, next) => {
-    //esta arrow function recibe un esquema, con el property le decimos donde encontrar la información y por ende va a retornar un middleware de forma dinámica
-
-    //de dónde viene la información?, la obtenemos de un request y dinámicante le digo de donde sacar la información
-    //la información dentro de un request puede venir en varios lugares, depende de si es un post o un get
-    //para Post (informacion -> body) para Get (informacion -> params ó query)
-
     const data = req[property];
-    const { error } = schema.validate(data);
+    const { error } = schema.validate(data, { abortEarly: false });
 
     if (error) {
       next(boom.badRequest(error));
