0fabd370def5c65b8930033d7285565fb5264291
Fabián Orjuela <fabianorjuela@Fabians-MacBook-Pro.local>
2023-04-18 18:26:53 -0500

    feat: 🎸 Clase 09: instalacion-config de ORM sequelize

--- services/productService.js
+++ services/productService.js
@@ -1,14 +1,12 @@
 const { faker } = require('@faker-js/faker');
 const boom = require('@hapi/boom');
 
-const pool = require('../libs/postgresPool');
+const sequelize = require('../libs/sequelize');
 
 class ProductsService {
   constructor() {
     this.products = [];
     this.generate();
-    this.pool = pool;
-    this.pool.on('error', (err) => console.error(err));
   }
 
   generate() {
@@ -35,8 +33,11 @@ class ProductsService {
 
   async find() {
     const query = 'SELECT * FROM tasks';
-    const rta = await this.pool.query(query);
-    return rta.rows;
+    const [data, metadata] = await sequelize.query(query);
+    return {
+      data,
+      metadata,
+    };
   }
 
   async findOne(id) {
